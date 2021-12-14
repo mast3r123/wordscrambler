6b4883b7ea6939d6d5ccf60843a82f8c7f13fbf2
mast3r123 <abhishek.mast3r@gmail.com>
2021-12-14 12:50:25 -0500

    Alert controller to get input

--- Project3/ViewController.swift
+++ Project3/ViewController.swift
@@ -15,6 +15,8 @@ class ViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(promptForAnswer))
+        
         if let startUrl = Bundle.main.url(forResource: "start", withExtension: "txt") {
             if let startWords = try? String(contentsOf: startUrl) {
                 allWords = startWords.components(separatedBy: "\n")
@@ -32,6 +34,24 @@ class ViewController: UITableViewController {
         title = allWords.randomElement()
         usedWords.removeAll(keepingCapacity: true)
         tableView.reloadData()
+    }
+    
+    func submit(_ answer: String) {
+        
+    }
+    
+    @objc func promptForAnswer() {
+        let ac = UIAlertController(title: "Enter Answer", message: nil, preferredStyle: .alert)
+        ac.addTextField()
+        
+        let submitAction = UIAlertAction(title: "Submit", style: .default) {
+            [weak self, weak ac] _ in //add weak reference to prevent strong reference cycle
+            guard let answer = ac?.textFields?[0].text else { return }
+            self?.submit(answer)
+        }
+        
+        ac.addAction(submitAction)
+        present(ac, animated: true, completion: nil)
     }
 
 }
